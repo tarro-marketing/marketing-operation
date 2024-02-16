2731b4f6d6af6f78e3a89c5064fc28a906d1cd53
Yuka Chen <yjchen9596@gmail.com>
2024-02-15 17:03:59 -0800

    update

--- funnel_update/data_cleaning_combine_two_list.R
+++ funnel_update/data_cleaning_combine_two_list.R
@@ -1,16 +1,16 @@
+setwd("C:/Users/skt/Documents/PerformanceMarketing/direct-mail-2023")
+
 library(tidyverse)
 
 
 contact_campaign_report <- read_csv("funnel_update/contact_campaign_report.csv")
 marketing_mel_mql_report <- read_csv("funnel_update/marketing_mel_mql_report.csv")
 
-account_id_contact_campaign_report <- contact_campaign_report  |> 
-  drop_na(`Account ID`)  |> 
+contact_campaign_report <- contact_campaign_report  |> 
   mutate(`Mobile - Primary`= str_replace_all(`Mobile - Primary`, "[^\\d]", "")) |> 
   rename_with(~str_replace_all(.,"[:punct:]","")) |>
   rename_with(~ str_replace_all(., " ", "_"))  |>
   rename_with(~ paste0(., "_Contact_Report"))  
-
 
 marketing_mel_mql_report <- marketing_mel_mql_report |> 
   mutate(`Mobile - Primary`= str_replace_all(`Mobile - Primary`, "[^\\d]", "")) |> 
@@ -18,34 +18,56 @@ marketing_mel_mql_report <- marketing_mel_mql_report |>
   rename_with(~ str_replace_all(., " ", "_"))  |>
   rename_with(~ paste0(., "_SFDC"))
 
-marketing_account_id <- marketing_mel_mql_report %>% 
-  filter(is.na(Latest_Campaign_SFDC)) %>% 
-  drop_na(Account_ID_SFDC) %>% 
-  left_join(account_id_contact_campaign_report, 
-            by = c("Account_ID_SFDC" = "Account_ID_Contact_Report", "Mobile__Primary_SFDC" = "Mobile__Primary_Contact_Report")) 
+lead_na_counts <- map_df(marketing_mel_mql_report, ~sum(is.na(.))) |> 
+  pivot_longer( cols = everything(), names_to = "column_name", values_to = "counts")
+lead_ducplicates <- map_df(marketing_mel_mql_report, ~sum(duplicated(.))) |> 
+  pivot_longer( cols = everything(), names_to = "column_name", values_to = "counts")
 
-marketing_account_id |> 
-  select(Latest_Campaign_SFDC,Campaign_Name_Contact_Report, everything()) |> 
-  drop_na(Campaign_Name_Contact_Report)->x
-
-
-marketing_phone_number <- marketing_mel_mql_report %>% 
-  filter(is.na(Latest_Campaign_SFDC)) %>% 
-  drop_na(Mobile__Primary_SFDC) %>% 
-  left_join(account_id_contact_campaign_report, by = c("Mobile__Primary_SFDC" = "Mobile__Primary_Contact_Report"))
-
-marketing_phone_number |> 
-  select(Latest_Campaign_SFDC,Campaign_Name_Contact_Report, everything()) |> 
-  drop_na(Campaign_Name_Contact_Report)->y
+camp_rep_na_counts <- map_df(contact_campaign_report, ~sum(is.na(.))) |> 
+  pivot_longer( cols = everything(), names_to = "column_name", values_to = "counts")
+camp_rep_duplicates <- map_df(contact_campaign_report, ~sum(duplicated(.))) |> 
+  pivot_longer( cols = everything(), names_to = "column_name", values_to = "counts")
 
 
+rm(lead_ducplicates)
+rm(lead_na_counts)
+rm(camp_rep_na_counts)
+rm(camp_rep_duplicates)
 
-## matching account id and phone numbers match more than just phone numbers, 
-## so will use the first one as truth of source for updating past event
-## 
+campaign_report_join <- contact_campaign_report |> 
+  select(Campaign_Name_Contact_Report, Mobile__Primary_Contact_Report, Account_ID_Contact_Report) |> 
+  distinct(Account_ID_Contact_Report, .keep_all = TRUE)
 
-marketing_account_id_column_order <- marketing_account_id |> 
-  select(Created_Date_SFDC, Created_By_SFDC, Lead_Channel_SFDC, Lead_Category_SFDC, Lead_Source_SFDC, Lead_Status_SFDC,Unqualified_Reason_SFDC, `Opportunity_Created_Date_SFDC`,Opportunity_ID_SFDC ,Lead_Stage_SFDC,everything() )
+no_latest_campaign <- marketing_mel_mql_report |> 
+  filter(is.na(Latest_Campaign_SFDC)) |> 
+  left_join(campaign_report_join, 
+            by = c("Account_ID_SFDC" = "Account_ID_Contact_Report",
+                   "Mobile__Primary_SFDC" = "Mobile__Primary_Contact_Report")) |> 
+  mutate(campaign_name = coalesce(Latest_Campaign_SFDC, Campaign_Name_Contact_Report))
+
+has_latest_campaign <- marketing_mel_mql_report |>
+  drop_na(Latest_Campaign_SFDC) |> 
+  mutate(Campaign_Name_Contact_Report = "",
+         campaign_name = Latest_Campaign_SFDC)
+
+final_campaign_sfdc_lead = rbind(no_latest_campaign,has_latest_campaign)
+
+
   
-write_csv(marketing_account_id, "mkt_sfdc_lead_w_contact_report.csv")
+write_csv(final_campaign_sfdc_lead, "funnel_update/clean-data/final_campaign_sfdc_lead.csv")
+
+#########################
+
+
+duplicated_ids <- contact_campaign_report %>%
+  filter(duplicated(Account_ID_Contact_Report) | 
+           duplicated(Account_ID_Contact_Report, fromLast = TRUE)) %>%
+  pull(Account_ID_Contact_Report) %>%
+  unique()
+
+# Step 2: Filter the original dataframe to only include rows with duplicated Account_ID_Contact_Report
+duplicate_ids_contact <- contact_campaign_report %>%
+  filter(Account_ID_Contact_Report %in% duplicated_ids)
  
+
+write_csv(duplicate_ids_contact, "funnel_update/clean-data/duplicate_ids_contact.csv")
