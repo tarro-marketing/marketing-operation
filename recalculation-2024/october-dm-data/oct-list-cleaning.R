4988bd83eae36bc09f7b2dc874ded8a498ba217e
Yuka Chen <yjchen9596@gmail.com>
2024-02-19 22:34:07 -0800

    update october sam list

--- recalculation-2024/october-dm-data/oct-list-cleaning.R
+++ recalculation-2024/october-dm-data/oct-list-cleaning.R
@@ -14,7 +14,16 @@ rm(brizo_canada, brizo_us)
 
 
 sam_list_oct <- sam_list_oct |> 
-  select(-c("Address Check", "Name + City Check", "Contact Count", "Website Technology Vendors")) |> 
-  drop_na("Snowball Map"  )
+  select(-c("Address Check", "Name + City Check", "Contact Count", "Website Technology Vendors", `All Phone (Print Shop)`,"Contact Phone","Estimated Employees")) |> 
+  drop_na("Snowball Map") |> 
+  mutate(  `Business Phone`= str_replace_all(str_replace_all(`Business Phone`, "-", ""), "[^0-9]", "")) |> 
+  mutate(`Business Phone` = na_if(`Business Phone`, ""))
 
-write_csv(sam_list_oct, "recalculation-2024/sam-list/sam_list_october.csv")
+  
+
+map_df(sam_list_oct,~sum(is.na(.))) |> 
+  pivot_longer(cols = everything(), names_to = "NA_Counts", values_to = "Counts" )
+
+
+
+write_csv(sam_list_oct, "recalculation-2024/october-dm-data/sam_list_october.csv", na = "")
