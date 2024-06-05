c87dab1973373538f1f4020e5bd53f2486f89ebb
Yuka Chen <yjchen9596@gmail.com>
2024-06-05 16:42:50 -0700

    add marketing nurture queue summary

---
+++ marketing-nurture-queue/marketing-nurture-queue.R
@@ -0,0 +1,10 @@
+library(tidyverse)
+library(googlesheets4)
+library(keyring)
+
+client_secret_path <- keyring::key_get(service = "googlesheets4", username = "client_secret_path")
+email <- keyring::key_get(service = "googlesheets4", username = "email")
+
+# Configure and authenticate using retrieved credentials
+gs4_auth_configure(path = client_secret_path)
+gs4_auth(email = email, cache = TRUE)
