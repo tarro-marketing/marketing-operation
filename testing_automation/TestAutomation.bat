36cfabaf3c5e2bfe914996b9f2781310acf1a3f4
Yuka Chen <youjia.chen@wondersco.com>
2024-08-13 15:35:08 -0700

    updated

--- testing_automation/TestAutomation.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-for /f "tokens=2 delims==" %%i in ('wmic OS Get localdatetime /value') do set datetimef=%%i
-set datetimef=%datetimef:~0,8%_%datetimef:~8,2%-%datetimef:~10,2%-%datetimef:~12,2%
-
-echo "success" %datetimef% >> C:\Users\skt\Documents\Performance-Marketing\weekly-data\testing_automation\test.log
